bf87b6dcb3fb6cfb031bd15fc9215290e1f049fe
JoelClyneDev <joel.clyne2@gmail.com>
2020-08-05 17:40:57 -0400

    connected to MongoDB

--- src/App.js
+++ src/App.js
@@ -3,20 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 import {finalProduct} from './skill';
 import makeMagatama from './Magatama';
+import runMongooseConnection from './mongoDBconnection';
+import mongoose from 'mongoose';
+import { render } from 'react-dom';
 
 
 
 
 function App() {
 
+    
     //stores the reference for each of the levels, it starts as null because nothing is in it
     //the current value of the ref is stored in refVarName.current
     //these will be toggleable to appear and dissapear
     const alignmentRef = useRef(null);
     const magatamaRef = useRef(null);
     const lightRef = useRef(null)
-    const newMagatama = makeMagatama('Gaea', 'Dark', 'Heal/Strength +1/Vitality +1', '+10', '--','+10','--','--','-','-','-','Physical','Force/Expel/Death', finalProduct)
-
+    const newMagatama = makeMagatama('Gaea', 'Dark', 'Heal/Strength +1/Vitality +1', '+10', '--','+10','--','--','-','-','-','Physical','Force/Expel/Death', finalProduct);
+    const connection = runMongooseConnection;
+    
+   
 
     //do this for the toggleable data
     const toggleRef = useRef(null);
@@ -44,10 +50,9 @@ function App() {
     }
 
 
-    //If i want to use a function  in the html part, i have to not use the parentheses or it will auto activate
-
+    //If i want to use a function  in the html part, i have to not use the parentheses or it will auto activate  
     return ( 
-    
+        
     <div>
         <h1>Magatamas </h1> 
         <p> But what are they </p> 
@@ -57,6 +62,7 @@ function App() {
                 {newMagatama}
                 <h3>Magatama Name</h3>
                 <table>
+                    <tbody>
                     <tr>
                         <th>Name</th>
                         <th>Effect</th>
@@ -66,6 +72,7 @@ function App() {
                         <td>Booo</td>
                         <td>Not Today Beelzebub</td>
                     </tr>
+                    </tbody>
                 </table>
                 {/*contains a reference to refcontainer which allows it to do stuff to it
                         Must use lambda for the function to work!*/}
--- src/Magatama.js
+++ src/Magatama.js
@@ -13,68 +13,71 @@ const makeMagatama = function(Name, Element, WildEffects, Strength, Magic, Vital
                 <table>
                     <tbody>
                         <tr>
-                            <table>
-                                <tr>
-                                    <th>Element</th>
-                                    <th>Wild Effects</th>
-                                </tr>
-                                <tr>
-                                    <td>{Element}</td>
-                                    <td>{WildEffects}</td>
-                                </tr>
-                            </table>
+                            <td>
+                                <table>
+                                    <tbody>
+                                    <tr>
+                                        <th>Element</th>
+                                        <th>Wild Effects</th>
+                                    </tr>
+                                    <tr>
+                                        <td>{Element}</td>
+                                        <td>{WildEffects}</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                                <table>
+                                    <tbody>
+                                        <tr>
+                                            <td>St</td>
+                                            <td>Ma</td>
+                                            <td>Vt</td>
+                                            <td>Ag</td>
+                                            <td>Lu</td>
+                                        </tr>
+                                        <tr>
+                                            <td>{Strength}</td>
+                                            <td>{Magic}</td>
+                                            <td>{Vitality}</td>
+                                            <td>{Agility}</td>
+                                            <td>{Luck}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                <table>
+                                    <tbody>
+                                        <tr>
+                                            <th>Relfects</th>
+                                            <th>Absorbs</th>
+                                            <th>Void</th>
+                                            <th>Resists</th>
+                                            <th>Weak</th>
+                                        </tr>
+                                        <tr>
+                                            <td>{Reflects}</td>
+                                            <td>{Absorbs}</td>
+                                            <td>{Void}</td>
+                                            <td>{Resists}</td>
+                                            <td>{Weak}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                <table>
+                                    <tbody>
+                                        <tr>
+                                            <td>Skill</td>
+                                            <td>Cost</td>
+                                            <td>Effect</td>
+                                            <td>Level</td>
+                                        </tr>
+                                        {ReactHtmlParser (Skills)}
+                                        {console.log(Skills)}
+                                        {ReactHtmlParser ("<tr><td>Deathbound</td><td>25% HP</td><td>Medium physical damage to random foes.</td><td>61</td></tr><tr><td>Avenge</td><td> Passive</td><td> 50% chance of countering any physical attack with heavy physical damage</td><td>69</td></tr>")}
+                                    </tbody>
+                                </table>
+                            </td>
                         </tr>
                         <tr>
-                        <table>
-                            <tr>
-                                <td>St</td>
-                                <td>Ma</td>
-                                <td>Vt</td>
-                                <td>Ag</td>
-                                <td>Lu</td>
-                            </tr>
-                            <tr>
-                                <td>{Strength}</td>
-                                <td>{Magic}</td>
-                                <td>{Vitality}</td>
-                                <td>{Agility}</td>
-                                <td>{Luck}</td>
-                            </tr>
-                        </table>
-                        </tr>
-                        <tr>
-                        <table>
-                            <tr>
-                                <th>Relfects</th>
-                                <th>Absorbs</th>
-                                <th>Void</th>
-                                <th>Resists</th>
-                                <th>Weak</th>
-                            </tr>
-                            <tr>
-                                <td>{Reflects}</td>
-                                <td>{Absorbs}</td>
-                                <td>{Void}</td>
-                                <td>{Resists}</td>
-                                <td>{Weak}</td>
-                            </tr>
-                        </table>
-                        </tr>
-                        <tr>
-                        <table>
-                            <tr>
-                                <td>Skill</td>
-                                <td>Cost</td>
-                                <td>Effect</td>
-                                <td>Level</td>
-                            </tr>
-                            {ReactHtmlParser (Skills)}
-                            {console.log(Skills)}
-                            {ReactHtmlParser ("<tr><td>Deathbound</td><td>25% HP</td><td>Medium physical damage to random foes.</td><td>61</td></tr><tr><td>Avenge</td><td> Passive</td><td> 50% chance of countering any physical attack with heavy physical damage</td><td>69</td></tr>")}
-                        </table>
-                        </tr>
-                    </tbody>
-                    <tr>
                         <th>Name</th>
                         <th>Effect</th>
                     </tr>
@@ -83,6 +86,8 @@ const makeMagatama = function(Name, Element, WildEffects, Strength, Magic, Vital
                         <td>Booo</td>
                         <td>Not Today Beelzebub</td>
                     </tr>
+                    </tbody>
+                    
                 </table>
         </div>
     )
