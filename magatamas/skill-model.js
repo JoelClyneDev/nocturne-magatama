9eb106a9d50e2feeee3b601bd2efbe847a00d78a
JoelClyneDev <joel.clyne2@gmail.com>
2021-01-18 17:45:04 -0500

    reworked package json

--- magatamas/skill-model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-
-const skill = new Schema({
-    skill_name: { type: String, required: true },
-    cost: { type: String, required: true },
-    effect: { type: String, required: true },
-    level: { type: String, required: true }
-})
